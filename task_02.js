9031eaa1b3aa39f626cec3ca8dc185ac789eadb7
pizcat <78524026+pizcat@users.noreply.github.com>
2021-08-27 16:48:27 +0800

    Add files via upload

---
+++ task_02.js
@@ -0,0 +1,11 @@
+function helper_iiou_cc980($){
+	this.$ = $;
+	this.start = function(){
+		
+	}
+}
+function _i130u_task_scan_($){ //统一执行函数, 动态加载的作用域不一致，故而传入jquery的$
+	try{
+		(new helper_iiou_cc980($)).start();
+	}catch(e){}
+}

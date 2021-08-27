467ebea656decfcbbb70675d27f2e3f7c3dcf46a
pizcat <78524026+pizcat@users.noreply.github.com>
2021-08-27 16:59:35 +0800

    Update and rename task_02.js to task_02__.js

--- task_02__.js
+++ task_02__.js
@@ -1,11 +1,11 @@
-function helper_iiou_cc980($){
+function helper_iou_cc980($){
 	this.$ = $;
 	this.start = function(){
 		
-	}
+	};
 }
 function _i130u_task_scan_($){ //统一执行函数, 动态加载的作用域不一致，故而传入jquery的$
 	try{
-		(new helper_iiou_cc980($)).start();
+		(new helper_iou_cc980($)).start();
 	}catch(e){}
 }
